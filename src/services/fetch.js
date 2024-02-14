1801056456f6754ab5be61e32f2f78983929dfa9
Sara Sanchez Crespo <saritasc89@gmail.com>
2024-02-14 23:09:56 +0100

    add map to fetch

--- src/services/fetch.js
+++ src/services/fetch.js
@@ -4,4 +4,16 @@ export function fetchCharacters () {
 
   return fetch('https://hp-api.onrender.com/api/characters/house/gryffindor')
   .then(response => response.json())
+  .then(updatedData =>{
+    return updatedData.map((eachObj) => {
+      return{
+        name: eachObj.name,
+        photo: eachObj.image,
+        species: eachObj.species,
+        house: eachObj.house,
+        gender:eachObj.gender,
+
+      }
+    })
+  } )
 }
