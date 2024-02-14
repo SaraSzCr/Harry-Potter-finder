19800b507569ee9e4c525fe07244f228d88a8a63
Sara Sanchez Crespo <saritasc89@gmail.com>
2024-02-14 11:05:08 +0100

    add starter kit and install nodemodules

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import '../scss/App.scss'
+
+function App() {
+  return (
+    <div>
+      <header className="title">
+        <h1>Harry Potter</h1>
+      </header>
+    </div>
+  )
+}
+
+export default App
