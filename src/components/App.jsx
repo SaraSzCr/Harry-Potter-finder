e932e47f7c2b4f7530981b5251042dff468c4d6d
Sara Sanchez Crespo <saritasc89@gmail.com>
2024-02-16 12:29:40 +0100

    add ls

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,19 @@ import { useState, useEffect } from "react";
 import "../scss/App.scss";
 import CharacterList from "./CharacterList";
 import Filters from "./filters/Filters";
+import ls from "../services/localStorage";
 
 import { fetchCharacters } from "../services/fetch";
 
 function App() {
   const [characters, setCharacters] = useState([]);
 
+  //ls.get ("project", [])
+
   useEffect(() => {
     fetchCharacters().then((responseData) => {
       setCharacters(responseData);
+      ls.set ("characters", responseData);
     });
   }, []);
 
