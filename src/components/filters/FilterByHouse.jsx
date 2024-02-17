fc5dc5d02a1262add56192f011bad562396c57ea
Sara Sanchez Crespo <saritasc89@gmail.com>
2024-02-17 11:42:52 +0100

    Update github Pages.

--- src/components/filters/FilterByHouse.jsx
+++ src/components/filters/FilterByHouse.jsx
@@ -3,13 +3,13 @@ function FilterByHouse() {
     <div className="main__form--container">
       <label className="main__form--container--label" htmlFor="house">
         Selecciona la casa
-        <select>
+      </label>
+      <select>
           <option value="Gryffindor">Gryffindor</option>
           <option value="Hufflepuff">Hufflepuff</option>
           <option value="Ravenclaw">Ravenclaw</option>
           <option value="Slytherin">Slytherin</option>
         </select>
-      </label>
     </div>
   );
 }
